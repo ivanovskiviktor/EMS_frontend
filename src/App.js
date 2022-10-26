06a5f15836b7b808d17d2d4fdffb7cdac4335c8e
ivanovskiviktor <ivanovskiviktor3001@gmail.com>
2022-10-26 23:30:51 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+  import './App.css';
+  import React from 'react';
+  import Login from './components/Login/Login.js';
+  import Register from './components/Register/Register.js';
+  import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+  } from "react-router-dom";
+  import OldLogin from './components/Login/OldLogin.js';
+  class App extends React.Component {
+    constructor(props) {
+      super(props)
+
+    
+    }
+
+
+    render() {
+      return (
+        <Router>
+          <div className="App">
+            <Routes>
+                  
+                    <Route exact path='/' element={<OldLogin/>} />
+                    <Route exact path="/register" element={<Register/>} />
+              
+            </Routes>
+
+          </div>
+        </Router>
+      );
+    }
+  }
+
+  export default App;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,101 @@
+            import React, { Component } from "react";
+            import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+            import logo from '../../images/loginPhoto.jpg';
+            import TextField from '@mui/material/TextField';
+            import Button from '@mui/material/Button';
+            // import LoginIcon from '@mui/icons-material/Login.js';
+
+            export default class OldLogin extends Component {
+
+            render() {
+                return (
+                  <section
+                    class="vh-100"
+                    
+                  >
+                    <div
+                      class="container py-5 h-100"
+                    >
+                      <div class="row d-flex justify-content-center align-items-center h-100">
+                        <div class="col col-xl-10">
+                          <div class="card" style={{ borderRadius: "1rem" }}>
+                            <div class="row g-0">
+                              <div class="col-md-6 col-lg-5 d-none d-md-block">
+                                <img
+                                  src={logo}
+                                  alt="login form"
+                                  class="img-fluid"
+                                  style={{
+                                    borderRadius: "1rem 0 0 1rem",
+                                    width: "500px",
+                                    height: "671.5px",
+                                  }}
+                                />
+                              </div>
+                              <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                                <div class="card-body p-4 p-lg-5 text-black">
+                                  <form>
+                                    <div class="d-flex align-items-center mb-3 pb-1">
+                                      <i
+                                        class="fas fa-cubes fa-2x me-3"
+                                        style={{ color: "#ff6219" }}
+                                      ></i>
+                                    </div>
+
+                                    <h5
+                                      class="fw-normal mb-3 pb-3"
+                                      style={{ letterSpacing: "1px" }}
+                                    >
+                                      Најавете се на системот
+                                    </h5>
+                                    <br />
+                                    <div class="form-outline mb-4">
+                                      <TextField
+                                        required
+                                        className="form-control form-control-lg"
+                                        id="outlined-required"
+                                        label="Е-маил адреса"
+                                      />
+                                    </div>
+                                    <div class="form-outline mb-4">
+                                      <TextField
+                                        required
+                                        className="form-control form-control-lg"
+                                        id="outlined-required"
+                                        label="Лозинка"
+                                        type="password"
+                                      />
+                                    </div>
+
+                                    <div class="pt-1 mb-4">
+                                      <Button
+                                        variant="contained"
+                                        // endIcon={<LoginIcon />}
+                                      >
+                                        Најавете се
+                                      </Button>
+                                    </div>
+
+                                    <br />
+                                    <br />
+                                    <p
+                                      class="mb-5 pb-lg-2"
+                                      style={{ color: "#393f81" }}
+                                    >
+                                      Доколку се уште се немате регистрирано,{" "}
+                                      <a href="/register" style={{ color: "#393f81" }}>
+                                        регистрирајте се тука!
+                                      </a>
+                                    </p>
+                                  </form>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </section>
+                );
+            }
+            }
